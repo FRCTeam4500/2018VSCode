97d2335c6e2328b6e8501aa6ca15f19b430bccd8
Nicolas Newman <nnewman316@gmail.com>
2019-01-04 16:28:24 -0600

    Added classes

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -14,13 +14,24 @@ package frc.robot;
  * floating around.
  */
 public class RobotMap {
-  // For example to map the left and right motors, you could define the
-  // following variables to use with your drivetrain subsystem.
-  // public static int leftMotor = 1;
-  // public static int rightMotor = 2;
+	/*===================
+	 * Drivetrain
+	 *===================*/
+	
+    public final static int FLSPEEDPORT = 7, FLANGLEPORT = 10;
+	public final static int FRSPEEDPORT = 2, FRANGLEPORT = 1;
+	public final static int BLSPEEDPORT = 6, BLANGLEPORT = 5;
+	public final static int BRSPEEDPORT = 3, BRANGLEPORT = 4;
+	
+	public final static double L = 29.5;
+	public final static double W = 29.5;
+	public final static double COUNTPERDEG = 16.2539;
 
-  // If you are using multiple modules, make sure to define both the port
-  // number and the module. For example you with a rangefinder:
-  // public static int rangefinderPort = 1;
-  // public static int rangefinderModule = 1;
+	/*===================
+	 * Configuration
+	 *===================*/
+	
+	public final static double DEADZONE_XY = 0.2;
+	public final static double DEADZONE_Z = 0.4;
+	public final static int TIMEOUT = 0;
 }
